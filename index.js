48e5321eeac5f454085760b49123a9c3c872fea5
Hoàng Công Sơn <76770303+Mklop0098@users.noreply.github.com>
2024-10-03 03:04:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+
 mongoose
   .connect(process.env.ATLAS_URI, {
     useNewUrlParser: true,
